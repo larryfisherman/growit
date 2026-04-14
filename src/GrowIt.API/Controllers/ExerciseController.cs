d4416c3349d1f6385df32326df4d964e414b2d81
larryfisherman <szczepannowak87@gmail.com>
2026-04-14 19:49:07 +0200

    further cqrs adjusments

--- src/GrowIt.API/Controllers/ExerciseController.cs
+++ src/GrowIt.API/Controllers/ExerciseController.cs
@@ -1,6 +1,16 @@
+using GrowIt.Application.Exercises.Queries.GetExerciseList;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
 namespace GrowIt.API.Controllers;
 
-public class ExerciseController
+[ApiController]
+[Route("api/[controller]")]
+public class ExerciseController(IMediator mediator) : ControllerBase
 {
-    
+        [HttpGet]
+        public async Task<IActionResult> GetExercises(CancellationToken ct = default)
+        {
+            return Ok(await mediator.Send(new GetExerciseListQuery(), ct));
+        }
 }
